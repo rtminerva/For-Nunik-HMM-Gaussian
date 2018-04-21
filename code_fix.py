491d1ba0f810de2c661c528e832ba6b92a56a7a9
Nunik Nurhayati <Nunik Nurhayati@MSI>
2018-04-21 21:25:02 +0900

    Test 2

--- code_fix.py
+++ code_fix.py
@@ -6,10 +6,11 @@ from matplotlib import cm, pyplot as plt
 from hmmlearn.hmm import GaussianHMM, GMMHMM
 from hmmlearn.base import _BaseHMM
 from matplotlib._image import GAUSSIAN
+# from Cython.Includes.libcpp import pair
 
 "INPUT"
 #number of state
-n = 3
+n = 4
 #covariance type
 covar_type = "full"
 #number of iteration
@@ -77,57 +78,69 @@ for i in range(0,len(result)):
 x_i = []
 for i in result:
     x_i.append(i[3])
-x_i_min = x_i[:]
+x_i_plus = x_i[:]
 
-del x_i[-1]
-x_i_min.pop(0)
+del x_i[-1] #remove last element 
+x_i_plus.pop(0) #remove first element
 
 X_i = x_i[:]
-X_i_min = x_i_min[:]
-density = []
-k = 1
-while k == 1:
-    if len(X_i) > 0:
-        density.append([X_i[0], X_i_min[0], 1])
-        X_i.pop(0)
-        X_i_min.pop(0)
-        jj = []
-        for j in range(0, len(X_i)):
-            if X_i[j] == density[-1][0] and X_i_min[j] == density[-1][1]:
-                density[-1][-1] += 1
-                jj.append(j)
-        for j in jj:
-            X_i.pop(j)
-            X_i_min.pop(j)    
-    else:
-        k == 0   
+X_i_plus = x_i_plus[:]
+pairr = [[x_i[0],x_i_plus[0]]]
+for j in range(1, len(x_i)):
+    if [x_i[j],x_i_plus[j]] not in pairr:
+        pairr.append([x_i[j],x_i_plus[j]])
 
-print(x_i)
-print(x_i_min)
-print(density)
+density = []
+for j in range(0, len(pairr)):
+    density.append([pairr[j],0])
+    for k in range(0, len(x_i)):
+        if pairr[j][0] == x_i[k] and pairr[j][1] == x_i_plus[k]:
+            density[-1][-1] += 1
+
+print('x_i     ', x_i)
+print('x_i_plus', x_i_plus)            
+print('list of [x_i,x_i_plus]', pairr)
+print('List of [[x_i,x_i_plus], number of repetition]', density)
+
+# k = 1
+# while k == 1:
+#     if len(X_i) > 0:
+#         density.append([X_i[0], X_i_min[0], 1])
+#         X_i.pop(0)
+#         X_i_min.pop(0)
+#         jj = []
+#         for j in range(0, len(X_i)):
+#             if X_i[j] == density[-1][0] and X_i_min[j] == density[-1][1]:
+#                 density[-1][-1] += 1
+#                 jj.append(j)
+#         for j in jj:
+#             X_i.pop(j)
+#             X_i_min.pop(j)    
+#     else:
+#         k == 0   
 
 "Print RESULT"
 print("Record of all hidden state")
 print("**********************************")
-print("No.","   ","TIME Start"," - ","TIME End","    ","VALUE")
+print("No.","   ","TIME Start"," - ","TIME End","    ","VALUE","    ","Hidden State {}th")
 for i in range(0,len(result)):
-    print(i, "    ",result[i][0], " - ", result[i][1], "   ", result[i][2])
+    print(i, "    ",result[i][0], " - ", result[i][1], "   ", result[i][2], "   ", result[i][3])
 
    
-"Plot data and result"
-x_plot = []
-y_plot = []
-for i in result:
-    x_plot.append(i[0])
-    x_plot.append(i[1])
-    
-    y_plot.append(i[2])
-    y_plot.append(i[2])
-
-plt.figure(1)
-plt.title("hmm Gaussian method fitting result vs data")
-plt.plot(x,y, 'r')#, x,y, 'bo')
-plt.plot(x_plot, y_plot, 'k')
-# plt.savefig("diag101000") 
-plt.savefig("%s.png" % figname)
-plt.show()
+# "Plot data and result"
+# x_plot = []
+# y_plot = []
+# for i in result:
+#     x_plot.append(i[0])
+#     x_plot.append(i[1])
+#     
+#     y_plot.append(i[2])
+#     y_plot.append(i[2])
+# 
+# plt.figure(1)
+# plt.title("hmm Gaussian method fitting result vs data")
+# plt.plot(x,y, 'r')#, x,y, 'bo')
+# plt.plot(x_plot, y_plot, 'k')
+# # plt.savefig("diag101000") 
+# plt.savefig("%s.png" % figname)
+# plt.show()
