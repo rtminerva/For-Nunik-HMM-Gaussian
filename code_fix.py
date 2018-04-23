03a681c45f1e466ee831ccd63355217fee804284
Suzuki-lab <Suzuki-lab@windowsHPDM>
2018-04-23 16:22:13 +0900

    uu

--- code_fix.py
+++ code_fix.py
@@ -6,8 +6,11 @@ from matplotlib import cm, pyplot as plt
 from hmmlearn.hmm import GaussianHMM, GMMHMM
 from hmmlearn.base import _BaseHMM
 from matplotlib._image import GAUSSIAN
+<<<<<<< HEAD
 from matplotlib.mlab import bivariate_normal
 from mpl_toolkits.mplot3d import Axes3D
+=======
+>>>>>>> branch 'master' of https://github.com/rtminerva/NunikHMM-Gaussian.git
 # from Cython.Includes.libcpp import pair
 
 "INPUT"
@@ -76,6 +79,7 @@ x_i_plus = x_i[:]
 del x_i[-1] #remove last element 
 x_i_plus.pop(0) #remove first element
 
+<<<<<<< HEAD
 ##Create pair of hidden state
 # ## based on hidden state vestor
 # X_i = x_i[:]
@@ -106,7 +110,16 @@ for j in range(0, len(pairr)):
         if pairr[j][0] == x_i[k] and pairr[j][1] == x_i_plus[k]:
             density[-1][-1] += 1
     zz.append(density[-1][-1])
+=======
+X_i = x_i[:]
+X_i_plus = x_i_plus[:]
+pairr = [[x_i[0],x_i_plus[0]]]
+for j in range(1, len(x_i)):
+    if [x_i[j],x_i_plus[j]] not in pairr:
+        pairr.append([x_i[j],x_i_plus[j]])
+>>>>>>> branch 'master' of https://github.com/rtminerva/NunikHMM-Gaussian.git
 
+<<<<<<< HEAD
 "Analysis 2 : Total time domain"
 analysis_2 = []
 for i in range(0,n):
@@ -115,14 +128,53 @@ for i in range(0,n):
         if j[3] == i:
             analysis_2[-1][1] += 1
             analysis_2[-1][2] += j[4]
+=======
+density = []
+for j in range(0, len(pairr)):
+    density.append([pairr[j],0])
+    for k in range(0, len(x_i)):
+        if pairr[j][0] == x_i[k] and pairr[j][1] == x_i_plus[k]:
+            density[-1][-1] += 1
+
+print('x_i     ', x_i)
+print('x_i_plus', x_i_plus)            
+print('list of [x_i,x_i_plus]', pairr)
+print('List of [[x_i,x_i_plus], number of repetition]', density)
+
+# k = 1
+# while k == 1:
+#     if len(X_i) > 0:
+#         density.append([X_i[0], X_i_min[0], 1])
+#         X_i.pop(0)
+#         X_i_min.pop(0)
+#         jj = []
+#         for j in range(0, len(X_i)):
+#             if X_i[j] == density[-1][0] and X_i_min[j] == density[-1][1]:
+#                 density[-1][-1] += 1
+#                 jj.append(j)
+#         for j in jj:
+#             X_i.pop(j)
+#             X_i_min.pop(j)    
+#     else:
+#         k == 0   
+>>>>>>> branch 'master' of https://github.com/rtminerva/NunikHMM-Gaussian.git
 
 "Print RESULT"
 print("Record of all hidden state")
 print("**********************************")
+<<<<<<< HEAD
 print("No.","   ","TIME Start"," - ","TIME End","    ","VALUE","         ","Hidden State {}th","          ","Time domain")
+=======
+print("No.","   ","TIME Start"," - ","TIME End","    ","VALUE","    ","Hidden State {}th")
+>>>>>>> branch 'master' of https://github.com/rtminerva/NunikHMM-Gaussian.git
 for i in range(0,len(result)):
+<<<<<<< HEAD
     print(i, "    ",result[i][0], " - ", result[i][1], "   ", result[i][2], "   ", result[i][3], "         ", result[i][4])
+=======
+    print(i, "    ",result[i][0], " - ", result[i][1], "   ", result[i][2], "   ", result[i][3])
+>>>>>>> branch 'master' of https://github.com/rtminerva/NunikHMM-Gaussian.git
 
+<<<<<<< HEAD
 "Print All hidden state parameter"
 # print("Transition matrix")
 # print(model.transmat_)
@@ -192,3 +244,23 @@ ax.set_ylabel('Hidden State')
 ax.set_zlabel('Number')
 plt.savefig("%s.png" % figname2)
 plt.show()
+=======
+   
+# "Plot data and result"
+# x_plot = []
+# y_plot = []
+# for i in result:
+#     x_plot.append(i[0])
+#     x_plot.append(i[1])
+#     
+#     y_plot.append(i[2])
+#     y_plot.append(i[2])
+# 
+# plt.figure(1)
+# plt.title("hmm Gaussian method fitting result vs data")
+# plt.plot(x,y, 'r')#, x,y, 'bo')
+# plt.plot(x_plot, y_plot, 'k')
+# # plt.savefig("diag101000") 
+# plt.savefig("%s.png" % figname)
+# plt.show()
+>>>>>>> branch 'master' of https://github.com/rtminerva/NunikHMM-Gaussian.git
